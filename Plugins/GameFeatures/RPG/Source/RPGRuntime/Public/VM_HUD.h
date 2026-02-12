308260fd0be2ed8bb443d4b1a82b46d6f8b5703b
Andres <66284241+aeche95@users.noreply.github.com>
2026-02-12 00:49:44 -0500

    Added mutables, mvvm

---
+++ Plugins/GameFeatures/RPG/Source/RPGRuntime/Public/VM_HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "VM_HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGRUNTIME_API UVM_HUD : public UMVVMViewModelBase
+{
+	GENERATED_BODY()
+	
+};
